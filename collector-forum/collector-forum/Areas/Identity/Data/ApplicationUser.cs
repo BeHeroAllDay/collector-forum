26931b2799092d5cf6767c03c217002cb8366a3a
Piotrek <piotrek9790@gmail.com>
2021-02-11 22:28:24 +0100

    Some trash added

---
+++ collector-forum/collector-forum/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace collector_forum.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(25, MinimumLength = 3)]
+        public string Nickname { get; set; }
+        [PersonalData]
+        [Column(TypeName="nvarchar(100)")]
+        public string FirstName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
